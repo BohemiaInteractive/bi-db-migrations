f37334028b0047792aaf98ae6285fba9736b2b31
fogine <7884288+fogine@users.noreply.github.com>
2017-09-01 18:56:05 +0200

    in progress

--- bin/bi-db-migrate.js
+++ bin/bi-db-migrate.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-const yargs   = require('yargs');
-const path    = require('path');
-const fs      = require('fs');
-const _       = require('lodash');
-const Promise = require('bluebird');
+const yargs        = require('yargs');
+const path         = require('path');
+const fs           = require('fs');
+const _            = require('lodash');
+const Promise      = require('bluebird');
+const childProcess = require('child_process');
 
 const env     = process.env;
 const VERSION = require('../package.json').version;
@@ -24,6 +25,11 @@ module.exports.migrationStatusCmd = migrationStatusCmd;
  * @param _yargs - parser definition
  */
 var _yargs = null;
+/*
+ * @param ROOT - project root with initialized git repository
+ */
+var ROOT = null;
+var MIG_DIR = null;
 
 //run only if this module isn't required by other node module
 if (module.parent === null) {
@@ -87,10 +93,18 @@ if (module.parent === null) {
     }, seedCmd)
     .command(['seed:all'], 'Run every seeder', {
     }, seedAllCmd)
+    .option('dir', {
+        alias: 'd',
+        describe: 'Base directory path for migrations (relative to project root dir)',
+        default: 'migrations',
+        global: true,
+        type: 'string'
+    })
     .option('interactive', {
         alias: 'i',
         describe: 'if not enabled, it will NOT prompt the user for anything.',
-        default: true,
+        default: false,
+        global: true,
         type: 'boolean'
     })
     .option('verbose', {
@@ -98,27 +112,55 @@ if (module.parent === null) {
         describe: 'Dumps more info to stdout',
         default: 1,
         count: true,
+        global: true,
         type: 'boolean'
     })
     .version('version', 'Prints bi-service version', VERSION)
+    .strict(true)
     .help('h', false)
     .alias('h', 'help')
     .wrap(yargs.terminalWidth());
 
     const argv = _yargs.argv;
+
 }
 
 /**
- *
+ * @param {Object} argv
+ * @return {Promise}
  */
-function initSeedCmd() {
-    console.log(utils.getNearestRepository());
+function _init(argv) {
+    ROOT = utils.getNearestRepository();
+    MIG_DIR = path.resolve(ROOT + path.sep + argv.dir);
+
+    if (argv.v > 1) console.info('Project root: ' + ROOT);
+
+    if (typeof ROOT !== 'string') {
+        console.error('Failed to find git project root');
+        process.exit(1);
+    }
+
+    return utils.hasMigrationsStructure(ROOT, argv.dir).then(function(has) {
+        if (!has) {
+            return utils.initFS(ROOT, argv.dir);
+        }
+    });
 }
 
 /**
- *
+ * @param {Object} argv
+ * @return {Promise}
  */
-function initSchemaCmd() {
+function initSeedCmd(argv) {
+    return _initSeedOrSchema(argv, 'seed');
+}
+
+/**
+ * @param {Object} argv
+ * @return {Promise}
+ */
+function initSchemaCmd(argv) {
+    return _initSeedOrSchema(argv, 'schema');
 }
 
 /**
@@ -152,3 +194,41 @@ function migrateCmd() {
 }
 
 
+/**
+ * @param {Object} argv
+ * @param {String} subject - seed|schema
+ * @return {Promise}
+ */
+function _initSeedOrSchema(argv, subject) {
+    return _init(argv).then(function() {
+        let content = utils.generateSqlCommentFlags({
+            require: argv.require
+        });
+
+        let fPath = 'src' + path.sep + argv.table + path.sep + subject + '.sql';
+
+        try {
+            utils.createFile(fPath, content, MIG_DIR);
+            if (argv.v) {
+                console.log(`Created ${subject} file at:`);
+                console.log(MIG_DIR + path.sep + fPath);
+            }
+        } catch(e) {
+            if (e.code == 'EEXIST') {
+                console.error(`${subject} file already created at ${MIG_DIR + path.sep + fPath}`);
+                console.error('Can not overwrite.');
+                process.exit(1);
+            }
+            throw e;
+        }
+
+        if (argv.interactive) {
+            return childProcess.spawn($EDITOR, [
+                MIG_DIR + path.sep + fPath,
+            ], {
+                stdio: 'inherit'
+            });
+        }
+
+    });
+}
