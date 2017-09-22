5a185b0332072289c25f2e2471d042f8cb8d26cc
fogine <7884288+fogine@users.noreply.github.com>
2017-09-22 14:51:48 +0200

    API refactored so that all commands are wrapped into a `Migration` class so it's possible to construct multiple migration objects for multiple projects at once

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
-const path         = require('path');
-const fs           = require('fs');
-const _            = require('lodash');
-const Promise      = require('bluebird');
-const childProcess = require('child_process');
-const semver       = require('semver');
-const config       = require('bi-config');
-const Table        = require('easy-table');
+const Migration = require('./lib/migration.js');
 
-const env     = process.env;
-const VERSION = require('./package.json').version;
-const $EDITOR = env.EDITOR;
-
-const utils          = require('./lib/util.js');
-const MigrationError = require('./lib/error/migrationError.js');
-
-const _config = new config.Config;
-_config.initialize();
-
-/*
- * @param ROOT - project root with initialized git repository
- */
-var ROOT = null;
-var MIG_DIR = null;
 var CLI = false;
 
-module.exports = cliInterface;
-module.exports.initSeedCmd        = initSeedCmd;
-module.exports.initSchemaCmd      = initSchemaCmd;
-module.exports.initMigrationCmd   = initMigrationCmd;
-module.exports.seedCmd            = seedCmd;
-module.exports.seedAllCmd         = seedAllCmd;
-module.exports.migrateCmd         = migrateCmd;
-module.exports.migrationStatusCmd = migrationStatusCmd;
-module.exports.getConfig          = function() {
-    return _config;
-};
+module.exports           = cliInterface;
+module.exports.Migration = Migration;
 
+let mig = module.exports.migration = new Migration();
 
 function cliInterface(yargs) {
 
@@ -56,7 +26,7 @@ function cliInterface(yargs) {
             array: true,
             type: 'string'
         },
-    }, initSeedCmd)
+    }, mig.initSeedCmd)
     .command(['init:schema'], 'Creates a new schema file at $MIG_DIR/src/$TABLE/schema.sql and opens it with $EDITOR', {
         table: {
             alias: 't',
@@ -70,7 +40,7 @@ function cliInterface(yargs) {
             array: true,
             type: 'string'
         },
-    }, initSchemaCmd)
+    }, mig.initSchemaCmd)
     .command(['init:migration', 'init:mig'], 'Generates a new sql/js migration from the src/ table files', {
         type: {
             alias: 't',
@@ -85,7 +55,7 @@ function cliInterface(yargs) {
             default: _config.get('sequelize:dialect') || 'postgres',
             type: 'string'
         },
-    }, initMigrationCmd)
+    }, mig.initMigrationCmd)
     .command(['mig:status','migration:status'], 'List the status of migrations', {
         limit: {
             alias: 'l',
@@ -93,8 +63,8 @@ function cliInterface(yargs) {
             default: 2,
             type: 'number'
         },
-    }, migrationStatusCmd)
-    .command(['migrate'], 'Run pending migrations', {}, migrateCmd)
+    }, mig.migrationStatusCmd)
+    .command(['migrate'], 'Run pending migrations', {}, mig.migrateCmd)
     .command(['seed'], 'Run seeder for specified table', {
         table: {
             alias: 't',
@@ -102,7 +72,7 @@ function cliInterface(yargs) {
             required: true,
             type: 'string'
         },
-    }, seedCmd)
+    }, mig.seedCmd)
     .command(['seed:all'], 'Run every seeder', {}, seedAllCmd)
     .option('mig-dir', {
         describe: 'Base directory path for migrations (relative to project root dir)',
@@ -126,394 +96,4 @@ function cliInterface(yargs) {
         type: 'boolean'
     })
     .strict(true);
-}
-
-/**
- * make sure we have a project we can interact with and that file system
- * structure for migrations is in place
- *
- * @param {Object} argv
- * @return {Promise}
- */
-function _init(argv) {
-    return Promise.try(function() {
-        ROOT = utils.getNearestRepository();
-
-        if (argv.v > 1) console.info('Project root: ' + ROOT);
-
-        global.verbose = argv.v;
-
-        if (typeof ROOT !== 'string') {
-            return _exit('Failed to find git project root', 1);
-        }
-
-        return _inspectDir(argv, ROOT).then(function(has) {
-            if (!has) {
-                return utils.initFS(ROOT, argv['mig-dir']);
-            }
-        });
-    });
-}
-
-
-/**
- * @param {Object} argv
- * @param {String} projectRoot
- *
- * @return {Promise}
- */
-function _inspectDir(argv, projectRoot) {
-    MIG_DIR = path.resolve(projectRoot + path.sep + argv['mig-dir']);
-
-    if (!fs.existsSync(projectRoot + '/package.json')) {
-        return Promise.reject(new Error(`${projectRoot} isn't valid npm module, package.json not found`));
-    }
-    return utils.hasMigrationsStructure(projectRoot, argv['mig-dir']);
-}
-
-/**
- * @param {Object} argv
- * @return {Promise}
- */
-function initSeedCmd(argv) {
-    return _initSeedOrSchema(argv, 'seed');
-}
-
-/**
- * @param {Object} argv
- * @return {Promise}
- */
-function initSchemaCmd(argv) {
-    return _initSeedOrSchema(argv, 'schema');
-}
-
-/**
- *
- */
-function initMigrationCmd(argv) {
-    let npmPackage, migVersion, latestRelease, tags;
-
-    return _init(argv).then(function() {
-        npmPackage = require(path.resolve(ROOT + '/package.json'));
-        if (!semver.valid(npmPackage.version)) {
-            throw new Error(`"${npmPackage.version}" is not valid semver version`);
-        }
-        //fetch current git semver release tags
-        return utils.getGitTagList(ROOT);
-    }).then(function(tagList) {
-        tags = tagList;
-        migVersion = npmPackage.version;
-        //if current package.json version already has an equivalent git tag,
-        //add `development` prerelease flag to the generated migration
-        if (~tags.indexOf(npmPackage.version)) {
-            if (semver.prerelease(migVersion)) {
-                migVersion += '.';
-            } else {
-                migVersion += '-';
-            }
-            migVersion += 'development';
-        }
-        latestRelease = utils.getPreviousRelease(npmPackage.version, tags);
-        //fetches dirrectories from $MIG_DIR/src/*
-        return utils.fetchMigrationTables(MIG_DIR);
-    }).then(function(tables) {
-        let _tables = _.reduce(tables, function(out, val, key) {
-            //we are gonna work only with the most recent version of a table
-            //schema -> directories in $MIG_DIR/src/ can be version like:
-            //$TABLE_NAME_v1 $TABLE_NAME_v2 etc...
-            if (val instanceof Array) {
-                let table = _.clone(val[0]);
-                table.table = key;
-                out.push(table);
-            }
-            return out;
-        }, []);
-
-        //reads the actual schema.sql & seed.sql files and populates corresponding
-        //$table in _tables collection
-        return utils.populateMigrationDefinitions(_tables, latestRelease, ROOT);
-    }).then(function(tables) {
-        tables.forEach(function(table) {
-            let _seedRequires = utils.getRequiredTables(table.seedData);
-            let _schemaRequires = utils.getRequiredTables(table.schemaData);
-
-            table.requires = _.union(_seedRequires, _schemaRequires);
-            table.seedDataDelta = utils.getNewLines(table.oldSeedData, table.seedData);
-            table.schemaDataDelta = utils.getNewLines(table.oldSchemaData, table.schemaData);
-        });
-        tables = utils.filterAndSortTables(tables);
-
-        if (!tables.length && global.verbose > 1) {
-            console.info('No database changes detected. Creating empty migration file');
-        }
-
-        //wrap sql commands into a transaction and save on fs
-        let promise = utils.createMigration(
-            tables,
-            migVersion,
-            MIG_DIR,
-            argv.dialect,
-            argv.type
-        );
-
-        return promise.then(function(fPath) {
-            if (argv.verbose) {
-                console.info(`Created ${fPath}`);
-            }
-            return _openInEditorWhenInteractive(fPath, argv);
-        });
-    });
-}
-
-/**
- * @param {Object} argv
- */
-function migrationStatusCmd(argv) {
-    return Promise.try(function() {
-        const sequelize = require('./lib/sequelize.js');
-        const Migrations = sequelize.modelManager.getModel('migrations');
-
-        return Migrations.findAll({
-            order: [['id', 'DESC']],
-            limit: argv.limit
-        }).then(function(migrations) {
-            let t = new Table;
-            migrations.forEach(function(mig) {
-                t.cell('version', mig.version);
-                t.cell('status', mig.status);
-                t.cell('created_at', mig.created_at);
-                t.cell('note', mig.note);
-                t.newRow();
-            });
-            return _exit(t.toString(), 0);
-        }).catch(function(err) {
-            return _exit(err, 1);
-        });
-    });
-}
-
-/**
- * @param {Object} argv
- */
-function seedCmd(argv) {
-    return seedAllCmd(argv);
-}
-
-/**
- * @param {Object} argv
- */
-function seedAllCmd(argv) {
-    return Promise.try(function() {
-        const sequelize = require('./lib/sequelize.js');
-
-        //make sure cwd is a npm module and contains a folder with migrations
-        return _inspectDir(argv, process.cwd()).then(function(has) {
-            if (!has) {
-                return _exit(`${process.cwd()} doesn't have valid "migrations" folder`, 1);
-            }
-            return utils.fetchMigrationTables(MIG_DIR);
-        }).then(function(tables) {
-            let _tables = _.reduce(tables, function(out, val, key) {
-                if (val instanceof Array
-                    && (argv.table === undefined || key === argv.table)
-                ) {
-                    let table = _.clone(val[0]);
-                    table.table = key;
-                    out.push(table);
-                }
-                return out;
-            }, []);
-
-            return utils.populateMigrationDefinitionsFromFS(_tables, null, ROOT);
-        }).then(function(tables) {
-            tables.forEach(function(table) {
-                let _seedRequires = utils.getRequiredTables(table.seedData);
-                let _schemaRequires = utils.getRequiredTables(table.schemaData);
-
-                table.requires = _.union(_seedRequires, _schemaRequires);
-                table.seedDataDelta = table.seedData;
-            });
-            //leave only the tables whose data & schema have changed since
-            //previous release & sort tables so that those tables which are dependent
-            //on others come after them
-            tables = utils.filterAndSortTables(tables);
-
-            if (!tables.length) {
-                if (argv.verbose) {
-                    console.info('Nothing to seed.');
-                }
-                return _exit(null, 0);
-            }
-
-            let sql = '';
-            tables.forEach(function(table) {
-                sql += table.seedData;
-            });
-            //sql = sqlUtils[].main('', sql, Date.now());
-            sql = utils.renderTemplate(sequelize.options.dialect, {
-                seed: sql,
-                migName: 'seeder_' + Date.now()
-            });
-
-            if (argv.verbose) {
-                console.info('Seeding...');
-            }
-            if (argv.verbose > 2) {
-                console.info(sql);
-            }
-
-            return utils.migratePlainSql.call(sql, sequelize).then(function() {
-                if (argv.verbose) {
-                    console.info('Successfully seeded.');
-                }
-                return _exit(null, 0);
-            });
-        }).catch(function(err) {
-            return _exit(err, 1);
-        });
-    });
-}
-
-/**
- * @param {Object} argv
- */
-function migrateCmd(argv) {
-    return Promise.try(function() {
-        const sequelize = require('./lib/sequelize.js');
-        const Migrations = sequelize.modelManager.getModel('migrations');
-
-        return _inspectDir(argv, process.cwd()).then(function(has) {
-            if (!has) {
-                return _exit(`${process.cwd()} doesn't have valid "migrations" folder`, 1)
-            }
-            return utils.fetchMigrationState(Migrations);
-        }).then(function(version) {
-            let migrations = utils.fetchMigrationScripts(MIG_DIR, version);
-
-            return Promise.each(migrations, function(mig) {
-                let fn;
-
-                switch (mig.type) {
-                    case 'sql':
-                        let sql = fs.readFileSync(mig.path);
-                        fn = utils.migratePlainSql.bind(sql.toString());
-                        break;
-                    case 'js':
-                        fn = require(mig.path);
-                        break;
-                }
-
-                if (argv.verbose) {
-                    console.info(`Initializing ${mig.version} migration...`);
-                }
-
-                return utils.migrate(fn, mig.version, sequelize).then(function() {
-                    if (argv.verbose) {
-                        console.info(`${mig.version} migrated successfully.`);
-                    }
-                });
-            });
-        }).catch(function(err) {
-            return _exit(err, 1);
-        }).then(function() {
-            if (argv.verbose) {
-                console.info('All done.');
-            }
-            return _exit(null, 0);
-        });
-    });
-}
-
-/**
- * @param {String} fPath
- * @return {undefined}
- */
-function _openInEditorWhenInteractive(fPath, argv) {
-    if (argv.interactive && fPath) {
-        return childProcess.spawn($EDITOR, [
-            fPath,
-        ], {
-            stdio: 'inherit'
-        });
-    }
-}
-
-/**
- * creates sql file named seed.sql or schema.sql at corresponding location
- * for the db table
- *
- * @param {Object} argv
- * @param {String} subject - seed|schema
- * @return {Promise}
- */
-function _initSeedOrSchema(argv, subject) {
-    return _init(argv).then(function() {
-        let content = utils.generateSqlCommentFlags({
-            require: argv.require
-        });
-
-        let fName;
-
-        switch (subject) {
-            case 'schema':
-                fName = 'schema';
-                break;
-            case 'seed':
-                fName = 'data';
-                break;
-            default:
-                throw new Error(`Invalid subject ${subject}`);
-        }
-
-        let fPath = 'src' + path.sep + argv.table + path.sep + fName + '.sql';
-
-        try {
-            utils.createFile(fPath, content, MIG_DIR);
-            if (argv.v) {
-                console.log(`Created ${subject} file at:`);
-                console.log(MIG_DIR + path.sep + fPath);
-            }
-        } catch(e) {
-            if (e.code == 'EEXIST') {
-                return _exit(
-                    `${subject} file already created at ${MIG_DIR + path.sep + fPath} \nCan not overwrite.`
-                    , 1
-                );
-            }
-            throw e;
-        }
-
-        return _openInEditorWhenInteractive(MIG_DIR + path.sep + fPath, argv);
-    });
-}
-
-/**
- * @param {Error|String} message
- * @param {Integer} code
- * @throws {Error}
- * @return {undefined}
- */
-function _exit(message, code) {
-    if (CLI) {
-        let log = code > 0 ?
-            console.error.bind(console)
-            : console.info.bind(console);
-
-        if (message) {
-            message = message.toJSON ? message.toJSON() : message.toString();
-            return log(message);
-        }
-
-        return process.exit(code);
-    } else {
-        if (code > 0) {
-            if (subject instanceof Error) {
-                throw subject;
-            } else if (typeof subject === 'string') {
-                throw new Error(subject);
-            }
-        } else {
-            return message;
-        }
-    }
 }
